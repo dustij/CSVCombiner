8932ad2d83741a16bc29ffe37cf97ac30bd3ea41
dustij <knightlybyte@gmail.com>
2024-10-28 13:51:58 -0500

    directory selection, chooser dialog, displays path

--- src/main/java/com/dustijohnson/App.java
+++ src/main/java/com/dustijohnson/App.java
@@ -1,30 +1,81 @@
 package com.dustijohnson;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App extends Application
+{
+
+    private final static StringProperty sourceDirectory = new SimpleStringProperty("");
+    private Stage stage;
+
+    public static void main(String[] args)
+    {
+        launch();
+    }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage)
+    {
+        this.stage = stage;
+
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var scene = new Scene(createContents(), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    private Region createContents()
+    {
+        HBox results = new HBox(20, createSourceInput(), createSourceButton());
+        results.setAlignment(Pos.CENTER);
+        return results;
     }
 
+    private Node createSourceInput()
+    {
+        Label label = new Label("Source Directory: ");
+        TextField textField = new TextField("");
+        textField.textProperty().bindBidirectional(sourceDirectory);
+        HBox hBox = new HBox(6, label, textField);
+        hBox.setAlignment(Pos.CENTER);
+        return hBox;
+    }
+
+    private Node createSourceButton()
+    {
+        Button button = new Button("Choose Folder");
+        button.setOnAction(e -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle("Select Source Directory");
+            if (sourceDirectory.isEmpty().get()) {
+                directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+            } else {
+                // TODO: handle IllegalArgumentException when folder parameter is not valid
+                directoryChooser.setInitialDirectory(new File(sourceDirectory.get()));
+            }
+            File dir = directoryChooser.showDialog(stage);
+            if (dir != null) sourceDirectory.set(dir.toString());
+        });
+        return button;
+    }
 }
